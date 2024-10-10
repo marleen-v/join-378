4011592b77d421a09076c66a1511c5e0cf4d5ba9
Martin Reifschneider <mreifschneider@me.com>
2024-10-10 08:35:27 +0200

    Update boards.js

--- scripts/boards.js
+++ scripts/boards.js
@@ -1,5 +1,25 @@
+// Funktion zum dynamischen Laden der HTML-Datei
+function loadHTML(url, targetElementId) {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Netzwerk-Fehler beim Laden der HTML-Datei.');
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Den HTML-Inhalt in das Ziel-Element einfügen
+        //document.getElementById(targetElementId).innerHTML = data;
+        document.querySelector(targetElementId).innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Fehler:', error);
+      });
+  }
+
 function initBoard() {
-    let body = document.getElementById('')
+    let body = document.querySelector('body');
+    loadHTML('../html/board.html', 'body');
 }
 
 
