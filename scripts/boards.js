97e3926aceb03b1c82fd8a4bce9df35abfd9e33e
Martin Reifschneider <mreifschneider@me.com>
2024-10-11 09:02:38 +0200

    Add Task prototype

--- scripts/boards.js
+++ scripts/boards.js
@@ -29,7 +29,7 @@ function processHTML(htmlString) {
 
 
 async function initBoard() {
-    const htmlContent = await loadHTML('../html/board.html');
+    const htmlContent = await loadHTML('../html/add-task.html');
 
     if (htmlContent) {
         processHTML(htmlContent); // Den HTML-String an eine andere Funktion weiterleiten
