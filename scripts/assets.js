2501851ba6069db5f3dfe89a49d3d9b0e941a778
marleen-v <marleen.vaalbrock@gmail.com>
2024-10-11 11:22:58 +0200

    dropdownmenu js

--- scripts/assets.js
+++ scripts/assets.js
@@ -1,13 +1,18 @@
+
+let dropDownOpen;
+
 /**
  * This function is used to open and close the dropdown-menu
  *
  * 
  */
 function openCloseDropdownMenu(){
-    const dropdownRef = document.getElementById(".dropdown-menu");
-    if(dropdownRef.classList.contains("d_none")){
-        dropdownRef.classList.remove("d_none");
-    } else {
-        dropdownRef.classList.add("d_none");
-    }
+const dropdownRef = document.getElementById("dropdown-menu");
+ if (dropDownOpen) {
+    dropdownRef.classList.add("d_none");
+    dropDownOpen = false;
+  } else if (!dropDownOpen) {
+    dropdownRef.classList.remove("d_none");
+    dropDownOpen = true;
+  }
 }
