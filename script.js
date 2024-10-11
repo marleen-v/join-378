3cf2ea6d211ba3d6c11fb38facf88e895268b765
Martin Reifschneider <mreifschneider@me.com>
2024-10-11 09:21:59 +0200

    Update index.html
    clickable navigation from add task to board

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function init() {
     //nur zum testen
     initBoard();
 }
+
+
+async function loadBoard() {
+    const htmlContent = await loadHTML('../html/board.html');
+
+    if (htmlContent) {
+        processHTML(htmlContent); // Den HTML-String an eine andere Funktion weiterleiten
+    }    
+}
+
+
+async function loadAddTask() {
+    const htmlContent = await loadHTML('../html/add-task.html');
+
+    if (htmlContent) {
+        processHTML(htmlContent); // Den HTML-String an eine andere Funktion weiterleiten
+    }    
+}
