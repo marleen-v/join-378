d926dba1cbbf9aa669266df1110946858858947b
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-10-12 20:18:49 +0200

    template add with variables

--- scripts/summary.js
+++ scripts/summary.js
@@ -2,7 +2,7 @@ const FIREBASE_URL = 'https://join-378-default-rtdb.europe-west1.firebasedatabas
 const USERS_DIR = '/users';
 const TASKS_DIR = '/tasks';
 const CONTACTS_DIR = '/contacts';
-let tasksFromFirebase =  [
+let tasksFromFirebase = [
   {
   "Title": "Projektinitialisierung",
   "Description": "Initialisierung des JavaScript-Projekts mit npm und Git-Repository.",
@@ -86,20 +86,27 @@ let tasksFromFirebase =  [
 ];
 
 
+
 let currentUserFirstName = "Sofia";
 let currentUserLastName = "Müller";
 
 
 function initSummary(){
-  // loadData(TASKS_DIR);
-  document.getElementById("main_summary").innerHTML = getTemplateMainSummary();
+  // setTimeout((loadData(TASKS_DIR)), 3000);
+  showMainTemplate();
   showGreetingDependingOnDaytime();
 }
+
 
 async function loadData(path=""){
   let res = await fetch(FIREBASE_URL + path + ".json");
   let resToJson = await res.json();
   tasksFromFirebase = resToJson;
+}
+
+
+function showMainTemplate(){
+  document.getElementById("main_summary").innerHTML = getTemplateMainSummary();
 }
 
 
