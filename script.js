ab7101d9fe52ae2972ef268e51c1b52e104c8455
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-10-14 11:15:08 +0200

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const FIREBASE_URL = 'https://join-378-default-rtdb.europe-west1.firebasedatabas
 const USERS_DIR = '/users';
 const TASKS_DIR = '/tasks';
 const CONTACTS_DIR = '/contacts';
+let dataFromFirebase = [];
 let tasksFromFirebase = [];
 
 let currentUserFirstName = "Sofia";
@@ -23,3 +24,8 @@ function openAddTask() {
 }
 
 
+async function loadData(path=""){
+    let res = await fetch(FIREBASE_URL + path + ".json");
+    let resToJson = await res.json();
+    dataFromFirebase = resToJson;
+}
--- scripts/login.js
+++ scripts/login.js
@@ -1,22 +1,9 @@
-const FIREBASE_URL = 'https://join-378-default-rtdb.europe-west1.firebasedatabase.app/';
-const USERS_DIR = '/users';
-const TASKS_DIR = '/tasks';
-const CONTACTS_DIR = '/contacts';
-let dataFromFirebase = [];
-
 let emailInputRef = document.getElementById("emailInput");
 let passwordInputRef = document.getElementById("passwordInput");
 
 
 function init(){
   loadData(USERS_DIR);
-}
-
-
-async function loadData(path=""){
-  let res = await fetch(FIREBASE_URL + path + ".json");
-  let resToJson = await res.json();
-  dataFromFirebase = resToJson;
 }
 
 
