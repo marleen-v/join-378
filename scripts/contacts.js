96540989e4aa9ea109f772c1e62e453b1349d57d
Marleen Vaalbrock <mava@picture.local>
2024-10-17 21:02:07 +0200

    dialog version 2

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,48 +1,57 @@
 const contactListRef = document.getElementById("contact-container");
+const singleContactRef = document.getElementById("single-contact");
 
 // Dialog
 const dialog = document.querySelector("dialog");
 
-let dialogElements = [];
-
-
-// Contactform 
+// Dialog Contactform
 const dialogTitle = document.getElementById("dialog-title");
 const dialogSubline = document.getElementById("dialogSubline");
 const dialogColor = document.getElementById("dialogColor");
 const dialogInitials = document.getElementById("dialogInitials");
 const contactForm = document.getElementById("contactForm");
 
-const cancelBtn = document.getElementById('cancelBtn');
-const submitBtn = document.getElementById('submitBtn');
-const checkIcon = document.getElementById('checkIcon')
+// Dialog Contactform Buttons
+const cancelBtnTitle = document.getElementById("cancelBtnTitle");
+const cancelBtn = document.getElementById("cancelBtn");
+const submitBtnTitle = document.getElementById("submitBtnTitle");
+const checkIcon = document.getElementById("checkIcon");
 
-//Input Fields
-
+//Dialog Contactform Input Fields
 const inputNameRef = document.getElementById("c-name");
 const inputEmailRef = document.getElementById("c-email");
 const inputPhoneRef = document.getElementById("c-phone");
-
-
-
-
 
 let contactList = [];
 let currentContact = [];
 
 let firstName;
 let lastName;
+let initials;
 
-
-
-
-async function initContacts () {
+/**
+ * This function loads the contactdata from firebase and renders the contact list
+ */
+async function initContacts() {
   contactList = await loadData(CONTACTS_DIR);
   renderContacts();
 }
 
 /**
- * 
+ * This function renders the contacts on the page
+ */
+function renderContacts() {
+  contactListRef.innerHTML = "";
+  sortContactsByFirstName();
+
+  for (let index = 0; index < contactList.length; index++) {
+    const contact = contactList[index];
+    addNewContactSection(index);
+    contactListRef.innerHTML += getContactHTML(contact, index);
+  }
+}
+
+/**
  * This function renders the new contact-list
  */
 async function renderNewContact() {
@@ -51,26 +60,9 @@ async function renderNewContact() {
 }
 
 /**
- *
- * This function renders the contacts on the page
- */
-  function renderContacts() {
-  
-  contactListRef.innerHTML = "";
-  sortContactsByFirstName();
-  
-  for (let index = 0; index < contactList.length; index++) {
-    const contact = contactList[index];
-    addNewContactSection(index);
-    contactListRef.innerHTML += getContactHTML(contact, index);
-  }
-}
-
-
-/**
  * This function adds a new Letter-Section to the contactlist on the page
- * 
- * @param {*} index 
+ *
+ * @param {*} index
  */
 function addNewContactSection(index) {
   if (index == 0) {
@@ -87,147 +79,168 @@ function addNewContactSection(index) {
   }
 }
 
-
 /**
  * This function renders the information of a clicked contact
- * 
- * @param {*} index 
+ *
+ * @param {*} index
  */
 function showContactInfo(index) {
-  
-  const singleContactRef = document.getElementById("single-contact");
   currentContact = contactList[index];
   singleContactRef.innerHTML = "";
   singleContactRef.innerHTML = getContactInfoTemplate(index);
   toggleActiveBtnColor(index);
 }
 
+/**
+ * This function closes an opend contact info
+ */
 function closeContactInfo() {
-  const singleContactRef = document.getElementById("single-contact");
   singleContactRef.innerHTML = "";
 }
 
-function toggleActiveBtnColor (btnIndex){
+/**
+ * 
+ * @param {Number} btnIndex 
+ */
+function toggleActiveBtnColor(btnIndex) {
   const contactBtns = document.querySelectorAll(".single-contact-btn");
-  for (let index = 0; index <  contactBtns.length; index++) {
-    const element =  contactBtns[index];
+  for (let index = 0; index < contactBtns.length; index++) {
+    const element = contactBtns[index];
     if (element == contactBtns[btnIndex]) {
       element.classList.add("single-contact-btn-active");
-    }else{
+    } else {
       element.classList.remove("single-contact-btn-active");
     }
   }
 }
 
-
-function openContactDialog() {
- 
-  dialog.showModal();
-  
- 
-  
-}
-
-function closeDialog() {
-  dialog.close();
- 
-}
-
-/* function clearContactForm() {
-  contactForm.reset(); 
-  submitBtn.classList.add("inactiv-btn");
-  checkIcon.classList.add("inactive-color")
-} 
+/**
+ * This function opens the dialog
  */
-/* function checkForm() {
+function openContactDialog() {
+  dialog.showModal();
+}
+
+/**
+ * This function closes the dialog
+ */
+function closeContactDialog() {
+  dialog.close();
+  contactForm.reset();
+  dialogColor.classList.remove(currentContact.color);
+}
+
+/**
+ * This function validates the form
+ */
+function checkForm() {
   if (contactForm.checkValidity()) {
-      submitBtn.classList.remove("inactiv-btn");
-      checkIcon.classList.remove("inactive-color")
+    submitBtn.classList.remove("inactiv-btn");
+    checkIcon.classList.remove("inactive-color");
   } else {
     submitBtn.classList.add("inactiv-btn");
-    checkIcon.classList.remove("inactive-color");
+    checkIcon.classList.add("inactive-color");
   }
-} 
+}
 
-inputNameRef.addEventListener('input', checkForm);
-inputEmailRef.addEventListener('input', checkForm);  */
+inputNameRef.addEventListener("input", checkForm);
+inputEmailRef.addEventListener("input", checkForm);
 
+/**
+ * This function renders the dialog to add a contact
+ *
+ */
+function renderAddContactDialog() {
+  dialogTitle.innerHTML = "Add Contact";
+  dialogSubline.innerHTML = "Tasks are better with a team!";
+  dialogInitials.innerHTML =
+    '<img src="../assets/icons/person.svg" alt="" class="circle-icon" />';
+  cancelBtnTitle.innerHTML = "Cancel";
+  submitBtnTitle.innerHTML = "Create Contact";
+  dialogColor.classList.add("neutralColor");
+  cancelBtn.onclick = function () {
+    closeContactDialog();
+  };
+  contactForm.onsubmit = function () {
+    addNewContact();
+  };
 
-function openAddContactDialog(){
+  submitBtn.classList.add("inactiv-btn");
+  checkIcon.classList.add("inactive-color");
 
-  dialogElements = {
-    "onsubmit": "addNewContact()",
-    "title": "Add contact",
-    "subline" : "Tasks are better with a team!",
-    "color": "neutralColor",
-    "initials": '<img src="../assets/icons/person.svg" alt="" class="circle-icon" />',
-    "cancelBtn": "Cancel",
-    "onclick_cancelBtn": "closeDialog()",
-    "submitBtn": "Create Contact",
-    "inactiveBtn": "inactiv-btn",
-    "inactiveIcon": "inactive-color"
-  }
-
-  
-  dialog.innerHTML = contactDialogTemplateHtml();
-  
-  openContactDialog();
-} 
-
-function openEditContactDialog(index){
-  dialogElements = {
-    "onsubmit": "updateContactInfo(" + index + ")",
-    "title": "Edit contact",
-    "subline" : " ",
-    "color": currentContact.color,
-    "initials": currentContact.initials,
-    "cancelBtn": "Delete",
-    "onclick_cancelBtn": "deleteContact(" + index + ")",
-    "submitBtn": "Save",
-  }
-  
-  dialog.innerHTML = contactDialogTemplateHtml();
-  fillInputFields(index);
-  
-  
   openContactDialog();
 }
 
-async function addNewContact()  {
-  const inputNameRef = document.getElementById("c-name");
-  const inputEmailRef = document.getElementById("c-email");
-  const inputPhoneRef = document.getElementById("c-phone");
-  let initials;
+/**
+ * This function renders the dialog to edit a chosen contact
+ *
+ * @param {Number} index
+ */
+function renderEditContactDialog(index) {
+  dialogTitle.innerHTML = "Edit Contact";
+  dialogSubline.innerHTML = "";
+  cancelBtnTitle.innerHTML = "Delete";
+  submitBtnTitle.innerHTML = "Save";
+  dialogColor.classList.add(currentContact.color);
+  dialogInitials.innerHTML = currentContact.initials;
+  cancelBtn.onclick = function () {deleteContact(index)};
+  contactForm.onsubmit = function () {updateContactInfo(index)};
 
-  firstName = inputNameRef.value.split(' ')[0];
+  fillInputFields(index);
+
+  submitBtn.classList.remove("inactiv-btn");
+  checkIcon.classList.remove("inactive-color");
+
+  openContactDialog();
+}
+
+/**
+ * This function fills in
+ *
+ * @param {Number} index
+ */
+function fillInputFields(index) {
+  inputNameRef.value =
+    contactList[index].firstName + " " + contactList[index].lastName;
+  inputEmailRef.value = contactList[index].email;
+  inputPhoneRef.value = contactList[index].phone;
+}
+
+/**
+ * This function adds a new contact to firebase
+ *
+ */
+async function addNewContact() {
+  firstName = inputNameRef.value.split(" ")[0];
   firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-  lastName = inputNameRef.value.split(' ')[1];
-  initials = findInitials(initials); 
+  lastName = inputNameRef.value.split(" ")[1];
+  initials = findInitials(initials);
 
   contactList.push({
-    "email": inputEmailRef.value,
-    "firstName": firstName,
-    "initials": initials,
-    "lastName": lastName,
-    "phone": inputPhoneRef.value
+    email: inputEmailRef.value,
+    firstName: firstName,
+    initials: initials,
+    lastName: lastName,
+    phone: inputPhoneRef.value,
   });
 
   sortContactsByFirstName();
   addContactColor();
-  
+
   await putData(CONTACTS_DIR, contactList);
   renderNewContact();
- 
+
   setTimeout(() => {
-    btnIndex = contactList.findIndex(contact => contact.email === inputEmailRef.value);
+    btnIndex = contactList.findIndex(
+      (contact) => contact.email === inputEmailRef.value
+    );
     showContactInfo(btnIndex);
-  
-    closeDialog();
+
+    closeContactDialog();
   }, 200);
+}
 
- }
-
- /**
+/**
  * This function sorts the contacts alphabetically
  *
  * @param {Array} contacts - this is the json array with all contacts
@@ -244,81 +257,86 @@ function sortContactsByFirstName() {
   });
 }
 
-
- function findInitials(initials){
-  if(lastName !== undefined ){
+/**
+ * This function returns the initials of the name of a contact
+ *
+ * @param {*} initials
+ * @returns
+ */
+function findInitials(initials) {
+  if (lastName !== undefined) {
     initials = firstName.charAt(0) + lastName.charAt(0);
-  }else{
+  } else {
     initials = firstName.charAt(0);
     lastName = ""; // empty string, because if not, page shows undefined
   }
-  return initials
+  return initials;
 }
 
 /**
  * This function adds a color to each contact
- * 
- * @param {Number} index 
+ *
+ * @param {Number} index
  */
-function addContactColor(){
+function addContactColor() {
   for (let index = 0; index < contactList.length; index++) {
     let contactIndex = index % contactColors.length; // because there can be more contacts than colors in "contactColors"
     contactList[index].color = contactColors[contactIndex];
-}
+  }
 }
 
-
+/**
+ * This function deletes a contact
+ *
+ * @param {Number} index
+ */
 async function deleteContact(index) {
-  contactList.splice(index, 1); 
+  contactList.splice(index, 1);
   renderContacts();
-  closeContactInfo(); 
-  closeDialog();
-  await putData(CONTACTS_DIR, contactList); 
+  closeContactInfo();
+  closeContactDialog();
+  await putData(CONTACTS_DIR, contactList);
 }
 
+/**
+ * This function updates a contact info, after it was edited
+ *
+ * @param {Number} index
+ */
+async function updateContactInfo(index) {
+  firstName = inputNameRef.value.split(" ")[0];
+  firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+  lastName = inputNameRef.value.split(" ")[1];
+  initials = findInitials(initials);
 
+  contactList[index] = {
+    email: inputEmailRef.value,
+    firstName: firstName,
+    initials: initials,
+    lastName: lastName,
+    phone: inputPhoneRef.value,
+  };
 
-function fillInputFields(index) {
-    const nameInputField = document.getElementById("c-name");
-    const emailInputField = document.getElementById("c-email");
-    const phoneInputField = document.getElementById("c-phone");
-   
-   nameInputField.value = contactList[index].firstName + " " + contactList[index].lastName;
-   emailInputField.value = contactList[index].email;
-   phoneInputField.value = contactList[index].phone; 
-
-   
-   
-  }
- 
-
-  async function updateContactInfo(index){
-    let initials;
-    const inputNameRef = document.getElementById("c-name");
-    const inputEmailRef = document.getElementById("c-email");
-    const inputPhoneRef = document.getElementById("c-phone");
-
-    firstName = inputNameRef.value.split(' ')[0];
-    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
-    lastName = inputNameRef.value.split(' ')[1];
-    initials = findInitials(initials);
-    
-    contactList[index] = {
-      "email": inputEmailRef.value,
-      "firstName": firstName,
-      "initials": initials,
-      "lastName": lastName,
-      "phone": inputPhoneRef.value
-  }
   sortContactsByFirstName();
   addContactColor();
   await putData(CONTACTS_DIR, contactList);
   renderNewContact();
- 
+
   setTimeout(() => {
-    btnIndex = contactList.findIndex(contact => contact.email === inputEmailRef.value);
+    btnIndex = contactList.findIndex(
+      (contact) => contact.email === inputEmailRef.value
+    );
     showContactInfo(btnIndex);
-  
-    closeDialog();
+
+    closeContactDialog();
   }, 200);
-};
+}
+
+//to do:
+// active User markieren ... (ich)
+// email schon einmal vorhanden? Dann kann nicht submitted werden
+// springen zum neu angelegten Kontakt
+// Farben beim Öffnen des Dialogs stimmen nicht überein --> Klassen müssen beim Schließen immer entfernt werden
+// Animation
+
+function findAndMarkActiveUser() {}
