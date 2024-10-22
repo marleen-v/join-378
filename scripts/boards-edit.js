8fd84581b2f30f68824937c6971b6817d81be406
Martin Reifschneider <mreifschneider@me.com>
2024-10-22 11:03:28 +0200

    Update board -> edit

--- scripts/boards-edit.js
+++ scripts/boards-edit.js
@@ -142,6 +142,33 @@ function assignContact(taskId) {
     }
 }
 
+function getSubtaskMask() {
+    return /*html*/`
+        <div class="subtasks-add-box p-right-8px clickable">
+            <span class="mg-left-8px">Add new subtask</span>
+            <img class="click-item size-16px" src="../assets/icons/subtasks_plus.svg" alt="">
+        </div>
+    `;
+}
+
+
+function getSubtaskInput() {
+    return /*html*/`
+        <div class="subtasks-add-box subtask-input p-right-8px">
+            <input type="text">
+            <div class="size-16px flex justify-content-center click-item"><img src="../assets/icons/close.svg" alt=""></div>
+            <div class="divider"></div>
+            <div class="size-16px flex justify-content-center click-item mg-left-8px "><img class="filter-color-to-black" src="../assets/icons/check.svg" alt=""></div>
+        </div>
+    `;
+}
+
+
+function addSubtask(taskId) {
+    let container = document.querySelector('.detailed-task-card-subtasks');
+    container.innerHTML = getSubtaskInput();
+}
+
 
 export function getDetailedEditableCard(taskId) {
     return /*html*/`
@@ -186,21 +213,18 @@ export function getDetailedEditableCard(taskId) {
             </div>
             <div class="grid grid-rows-2 gap-8px mg-right-8px">
                 <span class="detailed-card-label">Assigned to:</span>
-                <div class="add-task-card-assigned-to grid grid-rows-2 gap-8px clickable">
-                    <div class="assign-to-select-box" onclick="assignContact('${taskId}')">
+                <div class="add-task-card-assigned-to grid grid-rows-2 gap-8px">
+                    <div class="assign-to-select-box p-right-8px clickable" onclick="assignContact('${taskId}')">
                         <span class="mg-left-8px">Select contacts to assign</span>
-                        <img src="../assets/icons/arrow_drop_downaa.svg" alt="">
+                        <img class="click-item size-16px" src="../assets/icons/arrow_drop_downaa.svg" alt="">
                     </div>
                     <div class="add-task-card-persons grid grid-rows-auto auto-overflow-y set-height-128px"></div>
                 </div>
             </div>
             <div class="grid grid-rows-2 gap-8px mg-right-8px">
                 <span class="detailed-card-label mg-top-8px">Subtasks</span>
-                <div class="detailed-task-card-subtasks flex">
-                    <div class="subtasks-add-box">
-                        <span class="mg-left-8px">Add new subtask</span>
-                        <img src="../assets/icons/subtasks_plus.svg" alt="">
-                    </div>
+                <div onclick="addSubtask('${taskId}')" class="detailed-task-card-subtasks flex">
+                    ${getSubtaskMask()}
                 </div>
             </div>
             </div>  
@@ -220,3 +244,4 @@ window.selectPriority = selectPriority;
 window.closeEdit = closeEdit;
 window.assignContact = assignContact;
 window.chooseContact = chooseContact;
+window.addSubtask = addSubtask;
